5d1a96cc9aa904229ad406f3e5d886d2072e696d
abdulrahman1121 <abdallrahmaelmi456@gmail.com>
2023-11-18 12:13:37 -0800

    Started navbar

--- src/com/Navbar.jsx
+++ src/com/Navbar.jsx
@@ -3,8 +3,26 @@ import React from 'react'
 export const Navbar = () => {
   return (
         <>
-        <h1>this the nav bar page</h1>
         
+        
+          <div className="cont">
+            <div className="logo">
+              <h2>LiveSync</h2>
+            </div>
+            <nav className="nav-items">
+              <ul>
+                <li>
+                <a href="#">NBA</a></li>
+                <li><a href="#">NFL</a></li>
+                <li><a href="#">MLB</a></li>
+                <li><a href="#">Soccer</a></li>
+                <li><a href="#">NHL</a></li>
+              </ul>
+            </nav>
+
+            </div>
+        
+       
         </>
   )
 }
